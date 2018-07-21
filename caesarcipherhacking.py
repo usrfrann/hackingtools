87aea1bc2d082f38f2106138cd4e4c4add089eed
Francois Sissoko <fsissoko007@yahoo.com>
2018-07-21 04:51:22 -0600

    Add files via upload
    Fixing error the formatting errors so that the Caesar cipher works by brute force looping through all the keys.

--- caesarcipherhacking.py
+++ caesarcipherhacking.py
@@ -1,42 +1,29 @@
-#WellAnEvovledCipher
-
 message = 'ftu5Iu5Iy I5qo4q6Iyq55msqL'
-SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456780 !?.'
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456780 !?.'
 
-for key in range(len(SYMBOLS)):
-	translate = ''
-	#Loop through each symbol in message:
-	#for symbol in SYMBOLS:
-	#	symbolIndex = SYMBOLS.find(symbol)
-	#	translatedIndex = symbolIndex - key
-	#
-	#	# Handle the wraparound:
-	#	if translatedIndex < 0:
-	#		translatedIndex = translatedIndex + len(SYMBOLS)
-	#
-	#	# Append the decrypted symbol:
-	#	translated = translated + SYMBOLS[translatedIndex]
-	#
-	#
-	# Loop through each symbol in message:
-		for symbol in message:
-			if symbol in SYMBOLS:
-				symbolIndex = SYMBOLS.find(symbol)
-				translatedIndex = symbolIndex - key
+# loop through every possible key
+for key in range(len(LETTERS)):
 
-				# Handle the wraparound:
-				if translatedIndex < 0:
-					translatedIndex = translatedIndex + len(SYMBOLS)
+    # It is important to set translated to the blank string so that the
+    # previous iteration's value for translated is cleared.
+    translated = ''
 
-				# Append the decrypted symbol:
-				translated = translated + SYMBOLS[translatedIndex]
+    # run the encryption/decryption code on each symbol in the message
+    for symbol in message:
+        if symbol in LETTERS:
+            symbolIndex = LETTERS.find(symbol) # get the number of the symbol
+            symbolIndex = symbolIndex - key
 
-			else:
-				# Append the symbol without encryption/decrypting:
-				translated = translated + symbol
+            # handles wrap-around if index is 26 or larger or less than 0
+            if symbolIndex < 0:
+                symbolIndex = symbolIndex + len(LETTERS)
 
-			# Display every possible decryption:
-			print('Key #%s: %s' % (key, translated))
+            # add number's symbol at the end of translated
+            translated = translated + LETTERS[symbolIndex]
 
-    #IDE IED what grammar say you?
-    #Yes tomorrow there is a tomorrow
+        else:
+            # just add the symbol without encrypting/decrypting
+            translated = translated + symbol
+
+    # display the current key being tested, along with its decryption
+    print('Key #%s: %s' % (key, translated))
